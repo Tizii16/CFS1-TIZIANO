513635c3f64d1cd49d5f790752a21dafb9b6ba95
Tizii16 <PolpadreTiziano16@gmail.com>
2024-02-29 22:00:20 -0300

    mi primer commit

---
+++ CursoProgramacion/Ejercicio1.ts
@@ -0,0 +1,7 @@
+console.log("Receta para Biscochuelo");
+console.log("Ingredientes");
+console.log("Harina, huevo, leche, azucar, aceite, esecia de vainilla");
+console.log("Preparacion");
+console.log("Ir mesclando Los Ingredientes uno por uno");
+console.log("Colocar en un Molde");
+console.log("Hornear");
